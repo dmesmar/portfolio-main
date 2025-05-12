c9a327dc3c1115555a71d9c19ab3324aa8698859
dmesmar <dariomesas808@gmail.com>
2025-05-12 18:36:04 +0200

    Issue: #10: Subido el TFG en los tres idiomas

--- libs/core-components/src/lib/portfolio/portfolio-footer.component.tsx
+++ libs/core-components/src/lib/portfolio/portfolio-footer.component.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link';
+import { getCurrentLanguage } from '../language-configurator';
+import { ca, es, en } from '@dmesmar/i18n';
 
 export const PortfolioFooter = () => {
+    // Get the current language code
+  const langCode = getCurrentLanguage();
+  
+  // Map language code to language data
+  const langMap = { en, es, ca };
+  
+  // Get the language data based on current language
+  const lang = langMap[langCode];
   return (
     <div
       className="container d-flex align-items-center justify-content-center"
       data-aos="zoom-in"
     >
       <Link href="/portfolio" className="theme-btn">
-        Back to Portfolio Entries
+        {lang.portfolio.goBack}
       </Link>
     </div>
   );
